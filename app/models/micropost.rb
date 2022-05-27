4b566196fa77a141c280817f7b616b3ada4ca6b2
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2022-05-28 00:33:43 +0900

    修正

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -24,14 +24,13 @@ class Micropost < ApplicationRecord
   #     message: "should be less than 5MB"
   #   }
 
-  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
-                                    message: "must be a valid image format" },
-                    size:         { less_than: 5.megabytes,
-                                    message: "should be less than 5MB" }
-
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: "must be a valid image format" },
+                      size:         { less_than: 5.megabytes,
+                                      message: "should be less than 5MB" }
     # 表示用のリサイズ済み画像を返す
-  def display_image
-    image.variant(resize_to_limit: [500, 500])
-  end
+    def display_image
+      image.variant(resize_to_limit: [500, 500])
+    end
 
 end
