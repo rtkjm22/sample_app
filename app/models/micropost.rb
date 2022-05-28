d9e8f08d0f359e665dd47be2a7d369004959ba3f
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2022-05-29 02:26:50 +0900

    "74ddfc7"の状態に戻す

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -30,7 +30,8 @@ class Micropost < ApplicationRecord
                                       message: "should be less than 5MB" }
     # 表示用のリサイズ済み画像を返す
     def display_image
-      image.variant(resize:'500x500').processed
+      # image.variant(resize:'500x500').processed
+      image.variant(resize_to_limit: [500, 500])
     end
 
 end
